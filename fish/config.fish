a30e89fa9e1be61b92f2993005da93c943405372
James Little <jil@stripe.com>
2022-01-02 15:21:13 -0500

    Fish

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+source (rbenv init -|psub)
+source ~/stripe/space-commander/bin/sc-env-activate.fish
+functions -e fish_right_prompt
+zoxide init fish | source
+starship init fish | source
+
+function mkg
+    mkdir -p $argv[1] && cd $argv[1]
+end
+
+function gco
+    git checkout (git branch --all | fzf | tr -d '[:space:]')
+end
+
+abbr -a vim nvim
+abbr -a ls exa -hl --git
+abbr -a cd z
+
+abbr -a gcob git checkout -b
+abbr -a gp git pull
+abbr -a gpush git push
+abbr -a gs git status
